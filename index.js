b3727fe1d501ecce2a98246f8d81f9ed30c54e13
Roy Song <grafity11@gmail.com>
2025-11-12 15:15:16 +0900

    Add MongoDB read API and home page view

--- index.js
+++ index.js
@@ -1,12 +1,41 @@
 const express = require('express');
 const path = require('path');
-const app = express();
-// Render가 제공하는 PORT 환경변수 사용 (없을 때는 로컬 테스트용 3000)
-const PORT = process.env.PORT || 3000;
 const mongoose = require('mongoose');
+const app = express();
+
+const PORT = process.env.PORT || 3000;// MongoDB 연결
+
+const MONGO_URI = process.env.MONGO_URI; //mongodb+srv://<qdata>:<p@ngdang11>@clusterqzdb.4htxnn3.mongodb.net/<database>?retryWrites=true&w=majority';
+
+mongoose.connect(MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log('✅ MongoDB Connected'))
+.catch(err => console.error('❌ MongoDB Connection Error:', err));
+
+// 데이터 모델 정의 (Schema)
+const wrongwordSchema = new mongoose.Schema({
+  wordid: String,
+  wrongname: String,
+  rightname: String,
+});
+
+const WrongWord = mongoose.model('wrongword', wrongwordSchema, 'wrongword'); 
 
 // 정적 파일 경로
 app.use(express.static(path.join(__dirname, 'public')));
+
+// API 라우트 (데이터 조회)
+app.get('/api/words', async (req, res) => {
+  try {
+    const words = await WrongWord.find();
+    res.json(words);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: '서버 오류' });
+  }
+});
 
 // Home 페이지 라우트
 app.get('/', (req, res) => {
@@ -23,16 +52,6 @@ app.get('/product', (req, res) => {
   res.sendFile(__dirname + '/public/product.html');
 });
 
-// MongoDB 연결
-//const MONGO_URI = 'mongodb+srv://<qdata>:<p@ngdang11>@clusterqzdb.4htxnn3.mongodb.net/<database>?retryWrites=true&w=majority';
-const MONGO_URI = process.env.MONGO_URI;
-
-mongoose.connect(MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => console.log('✅ MongoDB Connected'))
-.catch(err => console.error('❌ MongoDB Connection Error:', err));
 
 // 서버 실행
 app.listen(PORT, () => {
